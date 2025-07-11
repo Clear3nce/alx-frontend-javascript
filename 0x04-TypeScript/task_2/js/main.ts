e19d80d35e84dd252a42add35cca4e1ffd3ad958
clearence Muluvhu <clearencemuluvhu@gmail.com>
2025-07-11 16:17:43 +0200

    addded  String literal types

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,79 @@
+// Define the interfaces
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+// Implement Director class
+class Director implements DirectorInterface {
+  workFromHome(): string {
+    return 'Working from home';
+  }
+
+  getCoffeeBreak(): string {
+    return 'Getting a coffee break';
+  }
+
+  workDirectorTasks(): string {
+    return 'Getting to director tasks';
+  }
+}
+
+// Implement Teacher class
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return 'Cannot work from home';
+  }
+
+  getCoffeeBreak(): string {
+    return 'Cannot have a break';
+  }
+
+  workTeacherTasks(): string {
+    return 'Getting to work';
+  }
+}
+
+// Create employee function
+function createEmployee(salary: number | string): Director | Teacher {
+  if (typeof salary === 'number' && salary < 500) {
+    return new Teacher();
+  }
+  return new Director();
+}
+
+
+// Creating functions specific to employees
+function isDirector(employee: Director | Teacher): employee is Director {
+  return (employee as Director).workDirectorTasks !== undefined;
+}
+
+function executeWork(employee: Director | Teacher): string {
+  if (isDirector(employee)) {
+      return employee.workDirectorTasks();
+  } else {
+      return employee.workTeacherTasks();
+  }
+}
+
+
+// Define the Subjects string literal type
+type Subjects = 'Math' | 'History';
+
+// Implement the teachClass function
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === 'Math') {
+    return 'Teaching Math';
+  } else if (todayClass === 'History') {
+    return 'Teaching History';
+  }
+  // This line is theoretically unreachable due to the type restriction
+  throw new Error('Invalid subject');
+}
